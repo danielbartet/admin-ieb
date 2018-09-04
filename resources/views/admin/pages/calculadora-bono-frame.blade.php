b178950b9736252cd92c58996ce1aebc015653b3
daniel bartet <dwbartet@gmail.com>
2018-09-03 21:30:15 -0300

    100%

--- resources/views/admin/pages/calculadora-bono-frame.blade.php
+++ resources/views/admin/pages/calculadora-bono-frame.blade.php
@@ -21,7 +21,7 @@
 
                 <div class="box-header with-border">
                     <div class="row">
-                        <div class="col-xs-12 col-md-9 col-sm-9 col-lg-9">
+                        <div class="col-xs-12 col-md-12 col-sm-12 col-lg-12">
                             <div class="panel panel-default">
                                 <div class="panel-body">
                                     <form id="calculadoraBonosForm" name="calculadoraBonosForm">
