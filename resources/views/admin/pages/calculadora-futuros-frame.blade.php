e24a7087ee63248d776c730168ebcfac6dae426b
Daniel Bartet <dwbartet@gmail.com>
2018-09-04 12:13:34 -0300

    Update calculadora-futuros-frame.blade.php
    desde github add js

--- resources/views/admin/pages/calculadora-futuros-frame.blade.php
+++ resources/views/admin/pages/calculadora-futuros-frame.blade.php
@@ -80,6 +80,7 @@
                 </div>
                 {{!! HTML::script('/assets/js/admin/admin.js', array('type' => 'text/javascript')) !!}
 {!! HTML::script('/assets/js/admin/finance.js', array('type' => 'text/javascript')) !!}
+{!! HTML::script('/assets/js/admin/iframeResizer.contentWindow.min.js', array('type' => 'text/javascript')) !!}
 
 
     <script type="text/javascript">
